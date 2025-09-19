90d3430cc98c18dbe4604e07c7cdb50c6f62ea2a
Andrey Sapeshko <andrdd17@gmail.com>
2025-09-19 15:59:30 +0300

    Создана страница home контроллер к ней и маршрутизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from catalog import views
 
 urlpatterns = [
     path('catalog/', views.catalog, name='catalog'),
+    path('home/', views.home, name='home'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def catalog(request):
     return render(request, 'catalog/catalog.html')
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
