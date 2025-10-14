fa1d55dc1587cc7bd7bb01954a13eb845bd7d6f1
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-14 14:53:25 +0300

    Добавлены классы контроллеров наследуемые от CBV и маршруты к ним

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from catalog import views
+from catalog.admin import Product
+from catalog.views import ProductDeleteView, ProductListView, ProductCreateView, ProductUpdateView, ProductDetailView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('catalog/', views.catalog, name='catalog'),
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:product_id>', views.product, name='product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('product/new/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/product_edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/product_delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('add_product/', views.add_product, name='add_product')
 ]
