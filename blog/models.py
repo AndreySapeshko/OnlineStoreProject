1e45ed6f148edf0b35221ab83afc41c3162be3ad
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-14 17:02:21 +0300

    Создана модель Post и контроллеры CRUD наследуемые от CBV

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст сообщения')
+    preview = models.ImageField(upload_to='blog/images/', verbose_name='Изображение')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    count_views = models.IntegerField()
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'text', 'preview', 'is_published', 'count_views']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'text', 'preview', 'is_published', 'count_views']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
