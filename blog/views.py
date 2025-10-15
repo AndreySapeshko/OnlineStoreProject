b160c9c557bb051aea66a6c7736f7c37fb206f11
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-15 04:47:38 +0300

    Добавлена фильтрация списка статей и счетчик просмотров

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
@@ -14,18 +15,28 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
+    def get_object(self, queryset=None):
+        obj = super().object(queryset)
+        if not obj.is_active:
+            raise Http404("Object not found")
+        obj.count_views += 1
+        return obj
+
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'text', 'preview', 'is_published', 'count_views']
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('blog:post_list')
+    success_url = reverse_lazy('blog:post_detail')
 
 
 class PostDeleteView(DeleteView):
