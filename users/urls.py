f92423dd27773614cd72338a34880a24edd785b1
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-21 18:48:39 +0300

    Созданы шаблоны входа и регистрации. Навбар реагирует на вход

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from catalog.urls import app_name
+from users.views import RegisterView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.html', next_page='catalog:home'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
+]
