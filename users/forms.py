ea849b9db54ca54197bb3c9ebe42f93690f98f8e
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-22 05:30:00 +0300

    Добавил стиль к форме регистрации

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ['email', 'username', 'avatar', 'phone_number', 'country']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for key, value in self.fields.items():
+            value.widget.attrs.update({'class': 'form-control'})
+
