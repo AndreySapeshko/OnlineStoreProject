d8c46ef5fab2f956179d144065678b9d7fc1d517
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-22 07:15:29 +0300

    Добавил шаблоны профиля и редактирования его представления и маршруты к ним

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 from .models import CustomUser
 
 
@@ -14,3 +16,14 @@ class CustomUserCreationForm(UserCreationForm):
         for key, value in self.fields.items():
             value.widget.attrs.update({'class': 'form-control'})
 
+
+class UserUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'avatar', 'phone_number', 'country']
+
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        for key, value in self.fields.items():
+            value.widget.attrs.update({'class': 'form-control'})
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 import os
 
-from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.views.generic import CreateView, UpdateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from dotenv import load_dotenv
 from django.contrib.auth import login
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserUpdateForm
+from .models import CustomUser
 
 
 class RegisterView(CreateView):
@@ -26,3 +29,21 @@ class RegisterView(CreateView):
         self.send_welcome_email(user.email)
         return super().form_valid(form)
 
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = UserUpdateForm
+    template_name = 'users/user_form.html'
+    success_url = reverse_lazy('users:user_detail')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserDetailView(DetailView):
+    model = CustomUser
+    template_name = 'users/user_detail.html'
+    context_object_name = 'user'
+
+    def get_object(self, queryset=None):
+        return self.request.user
