4180f4bd1d76707b4114b90701d34e751dda5d69
Andrey Sapeshko <andrdd17@gmail.com>
2025-10-22 07:15:29 +0300

    Добавил шаблоны профиля и редактирования его представления и маршруты к ним

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from catalog.urls import app_name
-from users.views import RegisterView
+from users.views import RegisterView, UserUpdateView, UserDetailView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html', next_page='catalog:home'), name='login'),
     path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('user/edit/', UserUpdateView.as_view(), name='user_edit'),
+    path('user/', UserDetailView.as_view(), name='user_detail')
 ]
